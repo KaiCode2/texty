5e02801f2873f6a6dae610b997faa3d7609e47ca
Kai Aldag <kaialdag@icloud.com>
2019-06-07 00:13:34 -0700

    project config

---
+++ Texty/Texty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Texty
+//
+//  Created by Kai Aldag on 2019-06-07.
+//  Copyright © 2019 Kai Aldag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
